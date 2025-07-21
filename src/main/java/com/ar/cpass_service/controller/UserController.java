d8b0c7340b95cd0548f7712a95e11ea6bce0bf75
A R Danish <helloardanish@gmail.com>
2025-07-22 00:44:59 +0530

    CORS error fix for /user/* API

--- src/main/java/com/ar/cpass_service/controller/UserController.java
+++ src/main/java/com/ar/cpass_service/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Slf4j
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/user")
 public class UserController {
